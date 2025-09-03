67ad5394129196cf83eca813e7252c07f42bd74c
Dan Dresser <ddresser05@gmail.com>
2025-09-03 15:51:40 -0500

    [week 1][day 1] feat: tracing framework added

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
-use poem::{Route, Server, listener};
+use poem::{EndpointExt, Route, Server, listener, middleware::Tracing};
 use poem_openapi::{OpenApi, OpenApiService, payload::PlainText};
 
+pub mod utils;
+
+#[derive(Debug)]
 struct EHRApi;
 
 #[OpenApi]
 impl EHRApi {
     #[oai(path = "/health", method = "get")]
+    #[tracing::instrument]
     async fn health_check(&self) -> PlainText<&'static str> {
+        tracing::info!("Health check requested");
         PlainText("EHR API is running")
     }
 }
@@ -23,10 +28,15 @@ impl EHRApp {
     }
 
     pub async fn run(&self) -> Result<(), std::io::Error> {
+        tracing::info!("Starting EHR API server on {}", self.app_address);
+
         let api_service = OpenApiService::new(EHRApi, "EHR API", "1.0")
             .server(format!("http://{}/api", self.app_address));
         let ui = api_service.swagger_ui();
-        let app = Route::new().nest("/api", api_service).nest("/docs", ui);
+        let app = Route::new()
+            .nest("/api", api_service)
+            .nest("/docs", ui)
+            .with(Tracing);
 
         let listener = listener::TcpListener::bind(&self.app_address);
         Server::new(listener).run(app).await
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use lgr_ehr::EHRApp;
+use lgr_ehr::{EHRApp, utils::tracing::init_tracing};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    init_tracing();
+
     let app = EHRApp::build("127.0.0.1:3000".into());
     app.run().await?;
 
